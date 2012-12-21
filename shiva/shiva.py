f398ff1b2bd16a4787a2a21caba22e22a21b8131
unrar <catbuntu@catbuntu.me>
2012-12-21 18:19:34 +0100

    Adding base for Shiva.

--- shiva/shiva.py
+++ shiva/shiva.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 #################
 # UnBot IRC Bot #
 #################
@@ -21,9 +24,19 @@ nick = 'unshiva'
 realname = 'Shiva 2012.12.18b'
 # Ident
 ident = 'shiva'
-# Canal
-chan = '#wikipedia-es-bots'
-##########################
+# Canal principal
+chan = '##bots-debug'
+# Canales a entrar
+chans = ["#sandyd", "#undb", "#undb-es", "#wikipedia-es-bots"]
+# Dueño
+owner = "wikimedia\/unrar"
+
+###################
+#    Funciones    #
+###################
+
+def privmsg(target, text):
+   irc.send ("PRIVMSG " + target + " :" + text + "\r\n")
 
 # Creamos el socket "irc"
 irc = socket.socket ( socket.AF_INET, socket.SOCK_STREAM )
@@ -33,22 +46,40 @@ print irc.recv ( 4096 )
 irc.send ( 'NICK ' + nick + '\r\n' )
 irc.send ( 'USER ' + ident + ' bla bla :' + realname + '\r\n' )
 irc.send ( 'JOIN ' + chan + '\r\n' )
+# Canales secundarios
+for tcanal in chans:
+   irc.send("JOIN " + tcanal + "\r\n")
 irc.send ( 'PRIVMSG ' + chan + ' :Hola mundo.\r\n' )
+
 while True:
    data = irc.recv ( 4096 )
-   if data.find ( 'PING' ) != -1:
-      irc.send ( 'PONG ' + data.split() [ 1 ] + '\r\n' )
-   if data.find ( '%shiva quit' ) != -1:
-      irc.send ( 'PRIVMSG ' + chan + " :Fine, if you don't want me\r\n" )
+   command = data.replace(chr(10), '')
+   command = command.replace(chr(13), '')
+   ex = command.split()
+   # achan => actual channel
+   achan = ex[2]
+   aparams = ex[3:]
+   aparams = ' '.join(aparams)
+   params = ex[4:]
+   rparams = ' '.join(params)
+   if ex[0] == "PING":
+	   irc.send ("PONG " + ex[1] + "\r\n")
+   if ex[1] == "PRIVMSG":
+      masktemp = ex[0].replace(chr(58), '')
+      # mask[0] => nick
+      # mask[1] => ident@host
+      mask = masktemp.split('!')
+      print ("@Debug: Mask[0] => " + mask[0] + "\n")
+      print ("@Debug: Mask[1] => " + mask[1] + "\n")
+      # Queries
+      if ex[2].lower() == nick.lower():
+         locparams = ' '.join(params)
+         tempsp = ex[3].split(':')
+         tempsp = tempsp[1:]
+         primera = ':'.join(tempsp)
+      if ex[3] == ":%test":
+         privmsg (achan, "Prueba recibida, gracias.")
+   if ' '.join(ex[3:5]).lower() == '%shiva quit':
+      privmsg (achan, "OK, habrá que irse...")
       irc.send ( 'QUIT\r\n' )
-   if data.find ( 'hola shiva' ) != -1:
-      irc.send ( 'PRIVMSG ' + chan + ' :Ya he dicho hola...\r\n' )
-   if data.find ( 'hey shiva' ) != -1:
-      irc.send ( 'PRIVMSG ' + chan + ' :Ya he dicho hola...\r\n' )
-   if data.find ( 'KICK' ) != -1:
-      irc.send ( 'JOIN ' + chan + '\r\n' )
-   if data.find ( 'cheese' ) != -1:
-      irc.send ( 'PRIVMSG ' + chan + ' :WHERE!!!!!!\r\n' )
-   if data.find ( 'slaps shiva' ) != -1:
-      irc.send ( 'PRIVMSG ' + chan + ' :This is the Trout Protection Agency. Please put the Trout Down and walk away with your hands in the air.\r\n' )
    print data
