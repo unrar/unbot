47e5e2e312bd9a4e66cb67372b6cb2bce3f25b7c
unrar <catbuntu@catbuntu.me>
2012-12-22 15:05:17 +0100

    Adding the &nas command

--- shiva/shiva.py
+++ shiva/shiva.py
@@ -8,8 +8,8 @@
 # Version: Shiva.2012.12.18b
 # Author: Catbuntu
 
-import socket
- 
+import socket, re
+from udb_connector import UNDB_Connector
 #############
 #  CONFIGS  #
 #############
@@ -41,6 +41,28 @@ def privmsg(target, text):
    irc.send ("PRIVMSG " + target + " :" + text + "\r\n")
 def sc (cmd):
    irc.send (cmd + "\r\n")
+def nick_exists_udb(nickn):
+   un = UNDB_Connector()
+   un_c = un.connect("nas.udb")
+   if (un_c == True):
+      udb_nas = open("cache.ucb", 'rb')
+      nas_found = False
+      for line in udb_nas:
+         nas_expl = line.split("~!")
+         for nas_vnick in nas_expl:
+            if (nas_vnick.rstrip() == nickn):
+               nas_found = True
+               nas_aname = nas_expl[0]
+      if nas_found != True:
+         return False
+      else:
+         return nas_aname
+      un.save("nas.udb")
+def unscape(stri):
+  return re.sub(r'\\(.)', r'\1', stri)
+      
+
+
 # Creamos el socket "irc"
 irc = socket.socket ( socket.AF_INET, socket.SOCK_STREAM )
 # Lo conectamos
@@ -113,5 +135,57 @@ while True:
          else:
             privmsg (achan, perror)
       
-      
+      # Nas command, to manage associations
+      if (ex[3].lower() == ":&nas") or (ex[3].lower() == ":&nariz"):
+         if len(ex) >= 5:
+            # There's an argument
+            udb_ne = nick_exists_udb(re.escape(mask[0]))
+            if udb_ne != False:
+               privmsg(achan, "Lo siento, el nick " + chr(3) + "12" + mask[0] + chr(15) + " ya está asignado a una cuenta.")
+            else :
+               uu = UNDB_Connector()
+               uu_cc = uu.connect("nas.udb")
+               if uu_cc == True:
+                  found = False
+                  file = open("cache.ucb", 'r')
+                  newfile = []
+                  for line in file:
+                     if line.split('~!')[0] == re.escape(ex[4]).rstrip():
+                        line =  line.rstrip() + "~!" + mask[0]
+                        found = True
+                     newfile.append(line.rstrip() + "\n")
+                  if found == False:
+                     nline = re.escape(ex[4]).rstrip() + "~!" + mask[0] + "\n"
+                     newfile.append(nline)
+                  file.close()
+                  fole = open("cache.ucb", "w")
+                  fole.write("".join(newfile))
+                  fole.close()
+                  privmsg(achan, "Asociado correctamente tu nick " + chr(3) + "12" + mask[0] + chr(15) + " a la cuenta " + ex[4] + ".")
+                  uu.save("nas.udb")
+         else:
+            # There's no argument
+            u = UNDB_Connector()
+            u_cc = u.connect("nas.udb")
+            if u_cc == False:
+               privmsg(achan, "Lo siento, el nick " + chr(3) + "12" + mask[0] + chr(15) + " ya está asignado a una cuenta.")
+            else:
+               udb_nas = open("cache.ucb", "r")
+               nas_nicks = []
+               nas_found = False
+               for line in udb_nas:
+                  nas_expl = line.split("~!")
+                  for nas_vnick in nas_expl:
+                     nas_vnick = unscape(nas_vnick)
+                     if (nas_vnick.rstrip() == mask[0]): nas_found = True
+                     nas_nicks.append(nas_vnick.rstrip())
+                  if nas_found == True:
+                     udb_dbo = nas_nicks.pop(0)
+                     privmsg(achan, "Tu cuenta " + nas_expl[0] + " tiene los siguientes nicks asociados: " + chr(3) + "12" + ' , '.join(nas_nicks) + chr(15) + ".")
+                     break 
+                  nas_nicks = []
+               if nas_found != True:
+                  privmsg(achan, "Tu nick no está asociado a ninguna cuenta.")
+               udb_nas.close()
+               u.save("nas.udb")
    print data
